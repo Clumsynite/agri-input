d3edec080daf83d235f7f50893cf1545872aa9a9
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-16 15:09:44 +0530

    created sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,54 @@
+import { NavLink } from "react-router-dom";
+import "../styles/Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <nav className="sidebar">
+      <div className="sidebar-content">
+        <NavLink
+          to="/"
+          exact
+          className="route-link"
+          activeClassName="router-link-active"
+          style={{
+            fontWeight: 700,
+            paddingBottom: 20,
+            paddingTop: 5,
+            fontSize: 18,
+          }}
+        >
+          Agri-Purchase Management
+        </NavLink>
+        <NavLink
+          to="/input"
+          activeClassName="router-link-active"
+          className="route-link"
+        >
+          Agri-Input Master Management
+        </NavLink>
+        <NavLink
+          to="/supplier"
+          activeClassName="router-link-active"
+          className="route-link"
+        >
+          Supplier Master Management
+        </NavLink>
+        <NavLink
+          to="/purchase-req"
+          activeClassName="router-link-active"
+          className="route-link"
+        >
+          Agri-Input Purchase Requests
+        </NavLink>
+        <NavLink
+          to="/tax"
+          activeClassName="router-link-active"
+          className="route-link router-link-bottom"
+        >
+          Tax Master Maintenance
+        </NavLink>
+      </div>
+    </nav>
+  );
+};
+export default Sidebar;

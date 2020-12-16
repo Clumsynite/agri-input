bf699d4c6ce8675e2555280845e51b0245a65ae8
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-16 19:33:03 +0530

    added template components for each sidebar link

---
+++ src/components/SidebarNavigation/PurchaseManagement.js
@@ -0,0 +1,9 @@
+const PurchaseManagement = () => {
+  return (
+    <div className="dummy-content">
+      <h2>Agri-Purchase Management</h2>
+    </div>
+  );
+};
+
+export default PurchaseManagement;
---
+++ src/components/SidebarNavigation/TaxMaster.js
@@ -0,0 +1,9 @@
+const TaxMaster = () => {
+  return (
+    <div className="dummy-content">
+      <h2>Tax Master Maintenance</h2>
+    </div>
+  );
+};
+
+export default TaxMaster;

bed479ee12ac24c1ac643bb9cf1c8334400d5a04
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-17 16:42:28 +0530

    added dummy buttons
    from sample

--- src/components/PurchaseNavigation/Executing.js
+++ src/components/PurchaseNavigation/Executing.js
@@ -1,7 +1,15 @@
+import "../../styles/Executing.css";
+
 const Executing = () => {
   return (
     <div className="executing">
-      <h2>Request Executing</h2>
+      <div className="dummy-button-group">
+        <button className="dummy-button">Undo Batch</button>
+        <button className="dummy-button">Assign Suppliers</button>
+        <button className="dummy-button dummy-button-disabled">
+          Generate PO
+        </button>
+      </div>
     </div>
   );
 };

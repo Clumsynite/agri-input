7bd1e463e4af7d09d4149a7c2491518162055e5f
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-17 17:32:18 +0530

    moved dummy buttons to another component

--- src/components/PurchaseNavigation/Executing.js
+++ src/components/PurchaseNavigation/Executing.js
@@ -3,13 +3,20 @@ import "../../styles/Executing.css";
 const Executing = () => {
   return (
     <div className="executing">
-      <div className="dummy-button-group">
-        <button className="dummy-button">Undo Batch</button>
-        <button className="dummy-button">Assign Suppliers</button>
-        <button className="dummy-button dummy-button-disabled">
-          Generate PO
-        </button>
-      </div>
+      <DummyButtonGroup />
+
+    </div>
+  );
+};
+
+const DummyButtonGroup = () => {
+  return (
+    <div className="dummy-button-group">
+      <button className="dummy-button">Undo Batch</button>
+      <button className="dummy-button">Assign Suppliers</button>
+      <button className="dummy-button dummy-button-disabled">
+        Generate PO
+      </button>
     </div>
   );
 };

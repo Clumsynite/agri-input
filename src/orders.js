28e7da78526f125dcb0e19a32cd28493f900a117
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-19 12:15:58 +0530

    added object to store order and products data
    taken from sample image

---
+++ src/orders.js
@@ -0,0 +1,123 @@
+const orders = [
+  {
+    key: "AGBUY13042020_1042",
+    batchedBy: {
+      name: "Alex Lee",
+      avatar:
+        "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png",
+      date: "Mon, 13 Apr, 10:42 AM",
+    },
+    products: [
+      {
+        key: 1,
+        category: "Seeds/Seedlings",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Chili Sakata 651 (1500 Seeds)",
+        quantity: 26,
+      },
+      {
+        key: 2,
+        category: "Nutrients/Fertilizer",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Acme Brand Fert A/B Set - 50kg",
+        quantity: 114,
+      },
+      {
+        key: 3,
+        category: "Pesticide/Fungicide",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Valiants ME (Methyl Eugenol) 30ml",
+        quantity: 40,
+      },
+      {
+        key: 4,
+        category: "Growing/Medium",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Acme Brand Cocopeat - 30Kg",
+        quantity: 20,
+      },
+      {
+        key: 5,
+        category: "Growing/Medium",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Acme Brand Cocohusk - 50Kg",
+        quantity: 50,
+      },
+      {
+        key: 6,
+        category: "Pesticide/Fungicide",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Decis (Delthametrin) 1L",
+        quantity: 4,
+      },
+    ],
+  },
+  {
+    key: "AGBUY09042020_1112",
+    batchedBy: {
+      name: "Alex Lee",
+      avatar:
+        "https://cdn4.iconfinder.com/data/icons/small-n-flat/24/user-alt-512.png",
+      date: "Thu, 9 Apr, 11:12 AM",
+    },
+    products: [
+      {
+        key: 1,
+        category: "Seeds/Seedlings",
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Chili Sakata 651 (1500 Seeds)",
+        quantity: 26,
+      },
+      {
+        key: 2,
+        category: "Nutrients/Fertilizer",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Acme Brand Fert A/B Set - 50kg",
+        quantity: 114,
+      },
+      {
+        key: 3,
+        category: "Pesticide/Fungicide",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Valiants ME (Methyl Eugenol) 30ml",
+        quantity: 40,
+      },
+      {
+        key: 4,
+        category: "Growing/Medium",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Acme Brand Cocopeat - 30Kg",
+        quantity: 20,
+      },
+      {
+        key: 5,
+        category: "Growing/Medium",
+
+        image:
+          "https://cdn4.iconfinder.com/data/icons/music-ui-solid-24px/24/help_info_question_faq-2-512.png",
+        description: "Acme Brand Cocohusk - 50Kg",
+        quantity: 50,
+      },
+    ],
+  },
+];
+
+export default orders

d988faf189389404432db326b6f3e68f0c43df20
Clumsy Knight <clusmsyknight@gmail.com>
2020-12-19 13:22:45 +0530

    added download icon and modified accordion-button content

--- src/components/Templates/OrderList.js
+++ src/components/Templates/OrderList.js
@@ -1,3 +1,6 @@
+import { InlineIcon } from "@iconify/react";
+import fileTypeExcel from "@iconify-icons/vscode-icons/file-type-excel";
+import downloadIcon from "@iconify-icons/icons8/download";
 import {
   Accordion,
   AccordionItem,
@@ -5,18 +8,43 @@ import {
   AccordionItemButton,
   AccordionItemPanel,
 } from "react-accessible-accordion";
-import ProductTable from "../Templates/ProductTable";
 import "react-accessible-accordion/dist/fancy-example.css";
+import ProductTable from "../Templates/ProductTable";
 
 const OrderList = (props) => {
   const { orders } = props;
+
+  const downloadName = (key) => {
+    return "RFQList_" + key.split("Y")[1];
+  };
 
   const orderList = orders.map((order) => {
     return (
       <AccordionItem className="accordion-item" key={order.key}>
         <AccordionItemHeading>
           <AccordionItemButton className="accordion-button">
-            {order.key}
+            <div className="accordion-button-key">{order.key}</div>
+            <span className="accordion-button-divider">|</span>
+            <div className="accordion-button-user">
+              <div className="user-avatar">
+                <img
+                  src={order.batchedBy.avatar}
+                  alt="User Avatar"
+                  width={40}
+                  height={40}
+                />
+              </div>
+              <div className="user-details">
+                <div className="user-name">{order.batchedBy.name}</div>
+                <div className="user-date">{order.batchedBy.date}</div>
+              </div>
+            </div>
+            <span className="accordion-button-divider">|</span>
+            <div className="accordion-button-csv">
+              <InlineIcon icon={fileTypeExcel} width={40} height={40} />
+              {downloadName(order.key)}
+              <InlineIcon icon={downloadIcon} width={40} height={40} />
+            </div>
           </AccordionItemButton>
         </AccordionItemHeading>
         <AccordionItemPanel className="accordion-panel">
@@ -26,7 +54,16 @@ const OrderList = (props) => {
     );
   });
   return (
-    <Accordion style={{ width: "82%", border: "none", padding: 20, transition: "0.5s ease-in-out" }}>{orderList}</Accordion>
+    <Accordion
+      style={{
+        width: "82%",
+        border: "none",
+        padding: 20,
+        transition: "0.5s ease-in-out",
+      }}
+    >
+      {orderList}
+    </Accordion>
   );
 };
 
